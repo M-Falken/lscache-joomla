0ed2b21dcb859839332c2f5598679bc4e073ea9b
M-Falken <gregory-roussel@orange.fr>
2026-07-16 15:04:41 +0200

    chore(release): bump version to 1.5.3 (J5/J6)
    CSRF token neutralization in joomla-script-options + hardened re-injection
    (superglobals + VirtueMart vRequest snapshot) warrant a version bump.

--- Joomla6/lscache_plugin/components/com_vminventory.php
+++ Joomla6/lscache_plugin/components/com_vminventory.php
@@ -5,7 +5,7 @@ defined('_JEXEC') or die;
  * @author    Grégory Roussel <siriusocteam@gmail.com>
  * @copyright 2026 Grégory Roussel. All rights reserved.
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
- * @version   1.5.2
+ * @version   1.5.3
  * @link      https://github.com/M-Falken
  *
  *  com_vminventory (https://extensions.joomla.org/extension/vm-inventory/)
